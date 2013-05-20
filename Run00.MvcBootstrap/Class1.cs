b1cf39e1dd3cc85d7a87eaa1b8d948729b45bcc8
Michael Huensch <mrh@run00.com>
2013-05-20 08:14:45 -0500

    Including generic factory and updating web config

---
+++ Run00.MvcBootstrap/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run00.MvcBootstrap
+{
+	public static class ServiceFactoryExtensions
+	{
+		public static object Create(this IGenericControllerFactory factory, Type serviceType)
+		{
+			return typeof(IGenericControllerFactory).GetMethod("Create")
+					.MakeGenericMethod(serviceType)
+					.Invoke(factory, new object[] { });
+		}
+	}
+}
---
+++ Run00.MvcBootstrap/IGenericControllerFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+namespace Run00.MvcBootstrap
+{
+	public interface IGenericControllerFactory
+	{
+		IController Create(type));
+		void Release(IController controller);
+	}
+}
