8d450555fc55576979872f9b636e2a31ffd8af3f
Michael Huensch <mrh@run00.com>
2013-05-19 21:34:34 -0500

    Updating missing code

---
+++ Run00.MvcBootstrap.TestArea/ExtensionsForHtmlHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Run00.MvcBootstrap
+{
+	public static class ExtensionsForHtmlHelper
+	{
+		public static MvcHtmlString Action(this HtmlHelper helper, string actionName, string controllerName, string areaName)
+		{
+			return helper.Action(actionName, controllerName, new { area = areaName });
+		}
+	}
+}
---
+++ Run00.MvcBootstrap.TestHost/ExtensionsForStream.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Run00.MvcBootstrap.TestHost
+{
+	public static class ExtensionsForStream
+	{
+		public static bool Matches(this Stream original, Stream compareTo)
+		{
+			var originalPosition = original.Position;
+			var compareToPosition = compareTo.Position;
+
+			if (original.CanSeek)
+				original.Position = 0;
+
+			if (compareTo.CanSeek)
+				compareTo.Position = 0;
+
+			var result = original.ContentMatches(compareTo);
+
+			if (original.CanSeek)
+				original.Position = originalPosition;
+
+			if (compareTo.CanSeek)
+				compareTo.Position = compareToPosition;
+
+			return result;
+		}
+
+		private static bool ContentMatches(this Stream original, Stream compareTo)
+		{
+			var originalByte = original.ReadByte();
+			var compareToByte = original.ReadByte();
+
+			if (originalByte != compareToByte)
+				return false;
+
+			if (originalByte == -1 && compareToByte == -1)
+				return true;
+
+			return ContentMatches(original, compareTo);
+		}
+	}
+}
